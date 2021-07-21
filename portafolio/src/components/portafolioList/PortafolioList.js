621acd0faa6f8ce7c382a53daa7e25f5cb8fe6ed
fran <francisco.vento@outlook.com>
2021-07-21 15:37:10 -0700

    se eliminan errores

--- portafolio/src/components/portafolioList/PortafolioList.js
+++
@@ -1,9 +0,0 @@
-import './portafolioList.scss'
-
-export default function PortafolioList({title, active, setSelected, id}) {
-  return (
-    <li className={active ? 'portafolioList active' : 'portafolioList'} onClick={()=> setSelected(id)} >
-      {title}
-    </li>
-  )
-}
